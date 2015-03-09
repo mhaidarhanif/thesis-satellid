e9a0543eccfbcbf8e1fcd920fac5b1edea6039c8
M Haidar Hanif <mhaidarhanif@gmail.com>
2015-03-09 09:30:22 +0700

    Include some external sources
    More graphics and listings.
    Also a bit of preparation assets.

---
+++ contents/chapters/literature-study/include/gherkin-executable.js
@@ -0,0 +1,11 @@
+describe("The particular feature title", function() {
+  describe("Specific situation description", function() {
+    it("will have an expected output description", function() {
+      // Setup: Given a condition, And an other condition
+      // Execute: When a performed action by someone
+      // Verify: Then an output is presented
+    });
+    it("will be another condition description", function() {});
+  });
+  describe("Other situation description", function() {});
+});
---
+++ contents/make.sh
@@ -0,0 +1,1 @@
+make; cp main.pdf amain.pdf; clear
