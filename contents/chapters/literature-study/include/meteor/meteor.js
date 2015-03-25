51374d54c3a97345a0987bc5c734399758cbe689
M Haidar Hanif <mhaidarhanif@gmail.com>
2015-03-25 23:36:21 +0700

    Fix long source code

--- contents/chapters/literature-study/include/meteor/meteor.js
+++ contents/chapters/literature-study/include/meteor/meteor.js
@@ -1,16 +1,19 @@
 if (Meteor.isClient) {
   Session.setDefault("counter", 0);
+
   Template.hello.helpers({
     counter: function() {
       return Session.get("counter");
     }
   });
+
   Template.hello.events({
     "click button": function() {
       Session.set("counter", Session.get("counter") + 1);
     }
   });
 }
+
 if (Meteor.isServer) {
   Meteor.startup(function() { // code to run on server at startup
   });
